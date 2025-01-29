9978050310c5f9baf6c49b1935b2189646dc5bb3
Komi Rodrigue KOUDAKPO <gabirusamaa@gmail.com>
2025-01-29 18:02:15 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_contributions/presentations/main_screen.dart';
-import 'package:project_contributions/presentations/screen.dart';
 
 void main() {
   
