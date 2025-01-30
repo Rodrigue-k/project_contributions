c537936488b87dffe8455392b1b9533318cb863c
Komi Rodrigue KOUDAKPO <gabirusamaa@gmail.com>
2025-01-30 19:27:50 +0000

    Update commit.dart

--- lib/models/commit.dart
+++ lib/models/commit.dart
@@ -1,18 +1,17 @@
-
 class Commit {
-  final String url;
-  final Map<String, dynamic> commit;
-  final Map<String, dynamic> author;
-  final List<dynamic>? files;
+        final String url;
+        final Map<String, dynamic> commit;
+        final List<dynamic>? files;
+        final Map<String, dynamic>? author;
 
-  Commit({required this.url, required this.commit, required this.author, this.files});
+        Commit({required this.url, required this.commit, this.files, this.author});
 
-  factory Commit.fromJson(Map<String, dynamic> json) {
-    return Commit(
-      url: json['url'],
-      commit: json['commit'],
-      author: json['author'],
-      files: json['files'],
-    );
-  }
-}
+        factory Commit.fromJson(Map<String, dynamic> json) {
+          return Commit(
+            url: json['url'] ?? '',
+            commit: json['commit'] ?? {},
+            files: json['files'],
+            author: json['author'],
+          );
+        }
+      }
